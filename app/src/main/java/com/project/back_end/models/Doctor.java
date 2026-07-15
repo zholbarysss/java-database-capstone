0a83dc0cd74bb61eaaf92ac50022d4ead12fa89f
zholbarysss <aitkazyyerali@gmail.com>
2026-07-16 04:10:13 +0500

    Doctor.java

--- app/src/main/java/com/project/back_end/models/Doctor.java
+++ app/src/main/java/com/project/back_end/models/Doctor.java
@@ -1,1 +1,41 @@
+package com.project.back_end.models;
 
+public class Doctor {
+
+    private Long id;
+    private String name;
+    private String specialization;
+
+    public Doctor() {
+    }
+
+    public Doctor(Long id, String name, String specialization) {
+        this.id = id;
+        this.name = name;
+        this.specialization = specialization;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+}
