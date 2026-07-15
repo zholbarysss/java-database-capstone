1cc4718443364c0d8b4bc751ffe2f05d9aea7848
zholbarysss <aitkazyyerali@gmail.com>
2026-07-16 04:11:22 +0500

    Appointment.java

--- app/src/main/java/com/project/back_end/models/Appointment.java
+++ app/src/main/java/com/project/back_end/models/Appointment.java
@@ -1,1 +1,54 @@
 
+package com.project.back_end.models;
+
+import java.time.LocalDateTime;
+
+public class Appointment {
+
+    private Long id;
+    private Long doctorId;
+    private Long patientId;
+    private LocalDateTime appointmentDate;
+
+    public Appointment() {
+    }
+
+    public Appointment(Long id, Long doctorId, Long patientId, LocalDateTime appointmentDate) {
+        this.id = id;
+        this.doctorId = doctorId;
+        this.patientId = patientId;
+        this.appointmentDate = appointmentDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(Long doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+
+    public LocalDateTime getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(LocalDateTime appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+}
