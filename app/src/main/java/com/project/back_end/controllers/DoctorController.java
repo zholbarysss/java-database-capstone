f708c0d51f805007425a2214502c2ced4b10ad8b
zholbarysss <aitkazyyerali@gmail.com>
2026-07-19 03:32:52 +0500

    Update DoctorController.java

--- app/src/main/java/com/project/back_end/controllers/DoctorController.java
+++ app/src/main/java/com/project/back_end/controllers/DoctorController.java
@@ -1,1 +1,24 @@
 
+package com.project.back_end.controllers;
+
+import com.project.back_end.models.Doctor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/doctors")
+public class DoctorController {
+
+    @GetMapping("/{user}/{doctorId}/{date}/{token}")
+    public List<String> getDoctorAvailability(
+            @PathVariable String user,
+            @PathVariable Long doctorId,
+            @PathVariable String date,
+            @PathVariable String token) {
+
+        Doctor doctor = new Doctor();
+
+        return doctor.getAvailableTimes();
+    }
+}
