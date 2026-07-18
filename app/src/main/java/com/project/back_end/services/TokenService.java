40c12b8b76a7f3a0df2d3a66986b21c8cd95440d
zholbarysss <aitkazyyerali@gmail.com>
2026-07-19 03:39:38 +0500

    Update TokenService.java

--- app/src/main/java/com/project/back_end/services/TokenService.java
+++ app/src/main/java/com/project/back_end/services/TokenService.java
@@ -1,1 +1,32 @@
+package com.project.back_end.services;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    private final String SECRET =
+            "ThisIsMyVerySecretKeyForJwtGeneration123456789";
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String email) {
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+}
