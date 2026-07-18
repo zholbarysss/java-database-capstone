ca5e698c25b41f537fa5363dd9270f20c09db5a6
zholbarysss <aitkazyyerali@gmail.com>
2026-07-19 03:40:23 +0500

    Update DoctorService.java

--- app/src/main/java/com/project/back_end/services/DoctorService.java
+++ app/src/main/java/com/project/back_end/services/DoctorService.java
@@ -1,1 +1,39 @@
 
+package com.project.back_end.services;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class DoctorService {
+
+    public List<String> getAvailableTimeSlots(Long doctorId, LocalDate date) {
+
+        return Arrays.asList(
+                "09:00",
+                "10:00",
+                "11:00",
+                "14:00",
+                "15:00"
+        );
+    }
+
+    public ResponseEntity<String> validateDoctorLogin(
+            String email,
+            String password
+    ) {
+
+        if ("doctor@test.com".equals(email)
+                && "password".equals(password)) {
+
+            return ResponseEntity.ok("Login successful");
+        }
+
+        return ResponseEntity.badRequest().body("Invalid credentials");
+    }
+
+}
